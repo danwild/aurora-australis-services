9c20438d05639f3d886b38b8794ed53f6972eabe
Daniel Wild <danwild@y7mail.com>
2016-01-04 17:15:07 +1100

    untangled request timeout trouble

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var httpRequest = require('http-request');
+//var http = require('http');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,78 +11,114 @@ var port = process.env.PORT || 3000;
 var config = require('./config.json');
 
 
-var noaaPoling = setInterval(function(){
+var magPolling = setInterval(function(){
 
-	// JSON POLLING
-	for(var i = 0; i < config.jsonEndpoints.length; i++){
+	var service = config.jsonEndpoints.mag;
+	console.log("requesting "+ service.url);
 
-		var service = config.jsonEndpoints[i];
+	request.get(service.url, function (error, response, body) {
 
-		request(service.url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
 
-			if (!error && response.statusCode == 200) {
-				console.log(body);
-				var jsonOut = fs.createWriteStream('data/json/'+ service.name +'.json');
-				jsonOut.write(body);
-				jsonOut.on('error', function(err) { console.log(err); });
-				jsonOut.end();
-			}
-			else {
-				console.log(error);
-			}
-		});
-	}
+			console.log("got: "+service.name);
+			console.log(body);
 
-	// TXT POLLING
-	for(var i = 0; i < config.txtEndpoints.length; i++){
+			var jsonOut = fs.createWriteStream('data/json/'+ service.name +'.json');
 
-		var service = config.txtEndpoints[i];
-
-		httpRequest.get({
-
-				url: service.url,
-				progress: function (current, total) {
-					console.log('downloaded %d bytes from %d', current, total);
-				}
-			},
-			'data/txt/' + service.name + service.extension, function (err, res) {
-
-				if (err) {
-					console.error(err);
-					return;
-				}
-
-				// these logs are always going to be small, so we'll just wop the lot
-				fs.readFile('data/txt/' + service.name + service.extension, {
-						encoding: 'UTF-8'
-					},
-
-					function (err, file) {
-
-						if (err) throw err;
-						var fileArr = file.toString().split(/\r?\n/);
-						var jsonOut = fs.createWriteStream('data/json/' + service.name + '.json');
-						var outData = [service.headers];
-
-						for(var i = 0; i < fileArr.length; i++){
-
-							// strip head and split into array
-							var lineOut = parseLineTxt(fileArr[i]);
-							if(lineOut && lineOut.length > 1){
-								outData.push(lineOut);
-							}
-						}
-
-						jsonOut.write(JSON.stringify(outData));
-						jsonOut.on('error', function(err) { console.log(err); });
-						jsonOut.end();
-						console.log("done!");
-					});
-
+			jsonOut.write(body);
+			jsonOut.on('error', function(err) {
+				console.log("JSON WRITE ERR");
+				fs.createWriteStream('error.txt').write(error).end();
+				console.log(err);
 			});
-	}
+			jsonOut.end();
+		}
+		else {
+			console.log("REQUEST ERR");
+			console.log(error);
+		}
+	});
 
 }, config.pollingIntervals.NOAA);
+
+var plasmaPolling = setInterval(function(){
+
+	var service = config.jsonEndpoints.plasma;
+	console.log("requesting "+ service.url);
+
+	request.get(service.url, function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+
+			console.log("got: "+service.name);
+			console.log(body);
+
+			var jsonOut = fs.createWriteStream('data/json/'+ service.name +'.json');
+
+			jsonOut.write(body);
+			jsonOut.on('error', function(err) {
+				console.log("JSON WRITE ERR");
+				fs.createWriteStream('error.txt').write(error).end();
+				console.log(err);
+			});
+			jsonOut.end();
+		}
+		else {
+			console.log("REQUEST ERR");
+			console.log(error);
+		}
+	});
+
+}, config.pollingIntervals.NOAA);
+
+var kpPolling = setInterval(function(){
+
+	var service = config.txtEndpoints.wingKp;
+
+	httpRequest.get({
+		url: service.url,
+		progress: function (current, total) {
+			console.log('downloaded %d bytes from %d', current, total);
+		}
+	},
+	'data/txt/' + service.name + service.extension, function (err, res) {
+
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		// these logs are always going to be small, so we'll just wop the lot
+		fs.readFile('data/txt/' + service.name + service.extension, {
+				encoding: 'UTF-8'
+			},
+
+			function (err, file) {
+
+				if (err) throw err;
+				var fileArr = file.toString().split(/\r?\n/);
+				var jsonOut = fs.createWriteStream('data/json/' + service.name + '.json');
+				var outData = [service.headers];
+
+				for(var i = 0; i < fileArr.length; i++){
+
+					// strip head and split into array
+					var lineOut = parseLineTxt(fileArr[i]);
+					if(lineOut && lineOut.length > 1){
+						outData.push(lineOut);
+					}
+				}
+
+				jsonOut.write(JSON.stringify(outData));
+				jsonOut.on('error', function(err) { console.log(err); });
+				jsonOut.end();
+				console.log("done!");
+			});
+
+	});
+
+}, config.pollingIntervals.NOAA);
+
 
 
 function parseLineTxt(line){
@@ -101,6 +138,12 @@ function parseLineTxt(line){
 		return lineOut;
 	}
 }
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.all('/*', function(req, res){
 
